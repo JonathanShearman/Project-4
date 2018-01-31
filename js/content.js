b4a89cc05264e02c703affb419ed1083e512c278
JonathanShearman <33021975+JonathanShearman@users.noreply.github.com>
2018-01-31 18:53:28 +0000

    fixed js issues

--- js/content.js
+++ js/content.js
@@ -23,17 +23,17 @@ $(document).ready(function() {
 function myFunction() {
 
     var input, filter, x, a, i;
-    input = document.getElementById('myInput');
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    x = document.getElementsByClassName('myImg');
-
+    x = document.getElementsByClassName("myImg");
 
     for (i = 0; i < x.length; i++) {
-        a = x[i].getAttribute('title')[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            x[i].style.display = '';
+        a = x[i].getAttribute("title");
+
+        if (a.toUpperCase().indexOf(filter) > -1) {
+            x[i].style.display = "";
         } else {
-            x[i].style.display = 'none';
+            x[i].style.display = "none";
         }
     }
 }
